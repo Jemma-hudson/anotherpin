149b2d7400d0d2bde7f3f2186eec1784f6277238
Jemma Hudson <jemmahudson_uk93@hotmail.co.uk>
2020-12-18 13:33:44 +0000

    set up cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,15 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
-  end
+   @product = Product.find(params[:id])
+
+   # if the product is already in the cart
+   @order_item = @current_cart.order_items.find_by(product: @product)
+
+   if @order_item.nil?
+     # if it doesn't exist in the cart
+     @order_item = @current_cart.order_items.new(product: @product, quantity: 1)
+   end
+
+ end
 end
